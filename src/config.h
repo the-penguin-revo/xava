144f197835869047457ebc5ae5031ae024783030
nikp123 <pavlica.nikola@gmail.com>
2019-01-11 23:23:53 +0100

    a bit of refactoring, will do the same for windows but later

--- src/config.h
+++ src/config.h
@@ -1,11 +1,11 @@
 struct config_params {
 	char *color, *bcolor, *raw_target, *audio_source, **gradient_colors, *shadow_color, *data_format;
 	char bar_delim, frame_delim, oddoneout;
-	double integral, gravity, ignore, sens, foreground_opacity, logScale, logBegin, logEnd, eqBalance; 
-	unsigned int lowcf, highcf, shdw, shdw_col;
+	double integral, gravity, ignore, sens, foreground_opacity, logScale, logBegin, logEnd, eqBalance;
+	unsigned int lowcf, highcf, shdw, shdw_col, fftsize, gradient_count, bgcol, col;
 	double *smooth;
-	int smcount, im, om, col, bgcol, autobars, stereo, is_bin, ascii_range,
- bit_format, gradient, fixedbars, framerate, bw, bs, autosens, overshoot, 
- set_win_props, w, h, fftsize, gradient_count;
-};
+	int smcount, im, om, autobars, stereo, is_bin, ascii_range,
+ bit_format, gradient, fixedbars, framerate, bw, bs, autosens, overshoot,
+ set_win_props, w, h;
+} p;
 void load_config(char configPath[255], char supportedInput[255], void* p);
